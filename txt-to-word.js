63173d1de110b31dbc43da144b6d1475ca16d6ef
Eve Bigaj <eve.bigaj@gmail.com>
2021-08-31 17:41:35 -0400

    add module for extracting words from .txt file

---
+++ txt-to-word.js
@@ -0,0 +1,30 @@
+// takes .txt file as input and converts to array of strings. 
+// input: numbered list within .txt file, each item in list is a new line
+// output: array of uncapitalized strings without #s or spaces
+
+var fs = require("fs");
+
+function txtToString(filePath){return fs.readFileSync(filePath, 'utf8')};
+
+function txtToWordArray(filePath){
+let string = txtToString(filePath);
+// converting string to array and progressively removing fluff:
+let noLineBreaks = string.split('\n');
+    //+/g means we're doing this iteratively, otherwise e.g. '10' would be replaced by '0'
+    noNumbers = noLineBreaks.map(string => string.replace(/[0-9]+/g,''));
+    noPeriods = noNumbers.map(string => string.replace('.', '') );
+    noSpaces = noPeriods.map(string => string.replace(/\s+/g, ''));
+    noCapitals = noSpaces.map(string => string.toLowerCase());
+    //for some reason, I'm getting an extra entry of length zero, so I'm removing it here:
+    noCapitals.pop(); 
+    return noCapitals
+
+}
+
+// tests
+// console.log('Current directory: ' + process.cwd()) checks current directory
+// console.log(txtToWordArray("./common-nouns.txt"))
+//console.log(txtToWordArray("./uncommon-nouns.txt"))
+
+
+   
